dcabee5b564e0e0030e9cfca2fa56f1e2eb155ad
davidjyacus <143612899+davidjyacus@users.noreply.github.com>
2025-05-22 17:53:08 +0200

    JS
    export/import/module
    started and checked innerHTML

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+import {menuArray} from "/data.js"
+
+
+
+const menuItem = `
+        <div class="menu-item">
+                <div class="emoji">${emoji}</div>
+                <div class="item-info">
+                <h3 class="item-name">${name}</h3>
+                <p class="item-ingredients">${ingredients}</p>
+                <p class="item-price>$${price}</p>
+                </div>
+        </div>
+        `
+
+
+
 /* Strategy
 
-     similar to Oldagram:
+     styling similar to Oldagram:
         1 section in HTML, iterate in JS for innerHtml
-
-      
 
 */
