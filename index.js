9ecf66c0fed2081a19a348afec60c33dbd3da6c1
davidjyacus <143612899+davidjyacus@users.noreply.github.com>
2025-05-23 09:43:57 +0200

    innerHtml
    running

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ import {menuArray} from "/data.js"
 
 
 
-const menuItem = `
-        <div class="menu-item">
-                <div class="emoji">${emoji}</div>
-                <div class="item-info">
-                <h3 class="item-name">${name}</h3>
-                <p class="item-ingredients">${ingredients}</p>
-                <p class="item-price>$${price}</p>
+const menuHtml = menuArray.map (function(menuItem) {
+                return `
+                <div class="menu-item">
+                        <div class="emoji">${menuItem.emoji}</div>
+                        <div class="item-info">
+                                <h3 class="item-name">${menuItem.name}</h3>
+                                <p class="item-ingredients">${menuItem.ingredients}</p>
+                                <p class="item-price>$${menuItem.price}</p>
+                        </div>
                 </div>
-        </div>
-        `
+                `
+        })
+
+document.getElementById("menu-item").innerHTML = menuHtml
 
 
 
